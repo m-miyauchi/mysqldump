8e2a9427b9f6a5d1c5396685673ce09359c99003
Masaki Miyauchi <masaki@miyauchi.me>
2016-03-09 02:15:12 +0900

    Update mysqldump.sh

--- mysqldump.sh
+++ mysqldump.sh
@@ -12,7 +12,7 @@ LOGFILESUM=`ls -1 ${OUTPUTDIR} | wc -l`
 OLDESTFILE=`ls -1 ${OUTPUTDIR} | head -1`
 FILENAME=${DBNAME}_${DATE}.sql
 
-mysqldump --single-transaction -u ${DBUSER} -p ${PASSWD} ${DBNAME} > ${OUTPUTDIR}/${FILENAME}
+mysqldump --single-transaction -u${DBUSER} -p${PASSWD} ${DBNAME} > ${OUTPUTDIR}/${FILENAME}
 
 if test ${LOGFILESUM} -ge 7;then
   rm ${OUTPUTDIR}/${OLDESTFILE}
