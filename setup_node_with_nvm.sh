bc1fbf1b0932118f989253f6f0687076ee6f22f7
Masaki Miyauchi <masaki@miyauchi.me>
2017-04-02 13:49:18 +0900

    Create setup_node_with_nvm.sh

---
+++ setup_node_with_nvm.sh
@@ -0,0 +1,18 @@
+!/bin/sh
+USER=`whoami`
+HOME=/home/${USER}
+# For Ubuntu
+# Install nvm with git
+echo "Starting setup nvm and gulp."
+echo "Start install nvm..."
+git clone https://github.com/creationix/nvm.git ${HOME}/.nvm
+cd ${HOME}/.nvm
+git checkout `git describe --abbrev=0 --tags`
+# Start nvm
+. ${HOME}/.nvm/nvm.sh
+echo "Start install Node.js..."
+nvm install stable
+echo "# Start nvm" >> ${HOME}/.profile
+echo "source ~/.nvm/nvm.sh" >> ${HOME}/.profile
+echo  "nvm use stable &" >> ${HOME}/.profile
+echo "Complete!"
