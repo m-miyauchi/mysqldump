22e8366d5b089e482464582f3379cc96069db31b
Masaki Miyauchi <masaki@miyauchi.me>
2017-07-12 21:29:40 +0900

    Update recording_cpu_mem_status.sh

--- recording_cpu_mem_status.sh
+++ recording_cpu_mem_status.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 CPUSTATUS=cpu_status.txt
 MEMORYSTATUS=memory_status.txt
+PROCESSSTATUS=process_status.txt
 DATE=`date +%Y%m%d%H%M`
-INTERVALTIME=1
+INTERVALTIME=5
 TESTNUM=12
 OUTPUTDIR=/tmp/${TESTNUM}/cpu_mem_status/${DATE}
 
@@ -10,12 +11,14 @@ if [ ! -e ${OUTPUTDIR} ]; then
   mkdir -p ${OUTPUTDIR}
 fi
 
-echo 'Recording CPU-load and Memory Usage...'
+echo 'Recording CPU-load, Process statys and Memory Usage...'
 echo 'Recorded data is here:' ${OUTPUTDIR}
 echo 'Exit recording: Ctrl + c'
 
 while true;
 do
+  echo `date "+%Y/%m/%d-%H:%M:%S"` >> ${OUTPUTDIR}/${PROCESSSTATUS};
+  ps aux >> ${OUTPUTDIR}/${PROCESSSTATUS};
   echo `date "+%Y/%m/%d-%H:%M:%S"` >> ${OUTPUTDIR}/${CPUSTATUS};
   uptime >> ${OUTPUTDIR}/${CPUSTATUS};
   echo `date "+%Y/%m/%d-%H:%M:%S"` >> ${OUTPUTDIR}/${MEMORYSTATUS};
