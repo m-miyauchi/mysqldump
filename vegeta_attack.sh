b2ddca93a555217d147522b231cc9e1434b027c4
MMiyauchi <masaki@miyauchi.me>
2017-09-03 21:16:00 +0900

    Update vegeta_attack.sh

--- vegeta_attack.sh
+++ vegeta_attack.sh
@@ -1,24 +1,44 @@
 #!/bin/sh
+# Vegeta Parameters #####
+DURATION=1
+TIMEOUT=60
+RATE=1
+#########################
 VEGETA=`which vegeta`
-LOGDIR=attack_results
-DATE=`date +%Y%m%d%H%M`
+OUTPUTDIR=attack_results
+DATE=`date +%Y%m%d%H%M%S`
 
 # Usage
-# 1. Install vegeta
-# https://github.com/tsenart/vegeta
-# 2. Prepare test-scenario for vegeta(format is .txt), and run command below.
-# echo test_scenario_A | ./vegeta_attack.sh
+# echo test_shenario.txt | ./vegeta_attack_a.sh
 
 if [ -p /dev/stdin ] ; then
-    TESTNAME=$(cat -)
-    echo "シナリオ: ${TESTNAME} を実行"
+    TESTFILE=$(cat -)
+    SCENARIO=$(echo ${TESTFILE} | sed "s/\.txt//" | sed "s/test_scenario\///")
+    echo "シナリオ: ${SCENARIO} を実行"
 else
-    echo "テストシナリオ名を入力してください"
+    echo "テストシナリオを入力してください"
 fi
 
-if [ ! -e ${LOGDIR} ]; then
-  mkdir -p ${LOGDIR}
-fi
+setup_directory() {
+  if [ ! -e ${1}/bin ]; then
+    mkdir -p ${1}/bin
+  fi
+  if [ ! -e ${1}/text ]; then
+    mkdir -p ${1}/text
+  fi
+  if [ ! -e ${1}/html ]; then
+    mkdir -p ${1}/html
+  fi
+}
+
+setup_directory ${OUTPUTDIR}/${SCENARIO}
+FILENAME=${SCENARIO}_${DATE}
 
 echo "攻撃を実行中…"
-cat ./test_scenario/${TESTNAME}.txt | ${VEGETA} attack -duration=1s --rate 1 | tee ${LOGDIR}/${TESTNAME}_${DATE}.bin | ${VEGETA} report
+cat ${TESTFILE} | ${VEGETA} attack -duration=${DURATION}s -timeout=${TIMEOUT}s --rate ${RATE} -header ${HEADER} | tee ${OUTPUTDIR}/${SCENARIO}/bin/${FILENAME}.bin | ${VEGETA} report
+echo "攻撃を完了"
+echo "攻撃結果をテキストファイルに出力"
+cat ${OUTPUTDIR}/${SCENARIO}/bin/${FILENAME}.bin | vegeta report -reporter=text > ${OUTPUTDIR}/${SCENARIO}/text/${FILENAME}.txt
+echo "攻撃結果をHTMLに出力"
+cat ${OUTPUTDIR}/${SCENARIO}/bin/${FILENAME}.bin | vegeta report -reporter=plot > ${OUTPUTDIR}/${SCENARIO}/html/${FILENAME}.html
+echo "シナリオ: ${SCENARIO} のテストを完了"
